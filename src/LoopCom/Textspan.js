204f2e11a1087d8401d1847e0fa2f79c4c9b0a6d
Ahmadayed22 <ahmad.ayed461@gmail.com>
2023-10-07 00:50:44 +0300

    reDeploy

--- src/LoopCom/Textspan.js
+++ src/LoopCom/Textspan.js
@@ -1,9 +1,24 @@
-const text = ['BoTTom', 'BoTTom', 'BEABEE',
-    'KOALA4D', 'Personal Project ', 'Sahem',
-    'HEMAM', 'KOALAD4D', 'ATLANTIC',
-    'MEPI', 'LOGO', 'MASS',
-    'RASHEED', 'UMMAT WAQF', 'UMMAT WAQF',
-    'HASAD School', 'UMMAT WAQF',
-    'Personal Project', 'Personal Project', 'STORY']
+const text = [
+  'BoTim',
+  'BoTim',
+  'BEABEE',
+  'KOALA4D',
+  'Personal Project ',
+  'Sahem',
+  'HEMAM',
+  'KOALAD4D',
+  'ATLANTIC',
+  'MEPI',
+  'LOGO',
+  'MASS',
+  'RASHEED',
+  'UMMAT WAQF',
+  'UMMAT WAQF',
+  'HASAD School',
+  'UMMAT WAQF',
+  'Personal Project',
+  'Personal Project',
+  'STORY',
+];
 
 export default text;
